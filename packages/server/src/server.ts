ba6c3679f72d2327f81b6bb858130c69c9f76694
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-09-24 10:16:09 -0300

    create appointments routes

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata'
+
 import express from 'express'
 import cors from 'cors'
 
@@ -8,8 +10,8 @@ import './database'
 const app = express()
 
 app.use(cors())
-app.use(routes)
 app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Server running on port 3333')
