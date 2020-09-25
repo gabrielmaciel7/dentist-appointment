18d43508f64a397d9211c5441e5f44725d05a208
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-09-25 09:22:38 -0300

    config multer

--- packages/server/src/routes/users.routes.ts
+++ packages/server/src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 /* eslint-disable camelcase */
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
 import { createUserValidator } from './../middlewares/usersValidator'
+import jwtAuthenticator from 'src/middlewares/jwtAuthenticator'
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 // eslint-disable-next-line prettier/prettier
 usersRouter.post('/', createUserValidator, async(request, response) => {
@@ -26,5 +30,15 @@ usersRouter.post('/', createUserValidator, async(request, response) => {
     return response.status(400).json({ error: error.message })
   }
 })
+
+usersRouter.patch(
+  '/avatar',
+  jwtAuthenticator,
+  upload.single('avatar'),
+  // eslint-disable-next-line prettier/prettier
+  async(request, response) => {
+    return response.json({ ok: true })
+  }
+)
 
 export default usersRouter
