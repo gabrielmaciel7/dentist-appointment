ca1f7bc8aaebde5396581892d79d99f5fc6e5aa1
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-10-01 16:40:27 -0300

    git init web

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,3 +1,16 @@
 import { createConnection } from 'typeorm'
 
-createConnection()
+createConnection({
+  name: 'default',
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: 5432,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  cli: {
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR
+  }
+})
