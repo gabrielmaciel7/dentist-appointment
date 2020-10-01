7c7747c00205e55667c3bddaa68e4afb5e66d154
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-10-01 09:58:11 -0300

    add server

---
+++ server/src/routes/sessions.routes.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { Router } from 'express'
+
+import AuthenticateUserService from '../services/AuthenticateUserService'
+import { authenticateUserValidator } from './../middlewares/usersValidator'
+
+const sessionsRouter = Router()
+
+sessionsRouter.post('/', authenticateUserValidator, async (request, response) => {
+  const { email, password } = request.body
+
+  const authenticateUser = new AuthenticateUserService()
+
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password
+  })
+
+  delete user.password
+
+  return response.json({ user, token })
+}
+)
+
+export default sessionsRouter
