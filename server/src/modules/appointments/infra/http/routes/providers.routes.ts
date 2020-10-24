b9d294a31810c3ab7629c169a80d0bea3762ada2
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-10-24 16:34:43 -0300

    add providers availability routes

--- server/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ server/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express'
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController'
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController'
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController'
+
 import jwtAuthenticator from '@modules/users/infra/http/middlewares/jwtAuthenticator'
 
 const providersRouter = Router()
+
 const providersController = new ProvidersController()
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController()
+const providerDayAvailabilityController = new ProviderDayAvailabilityController()
 
 providersRouter.use(jwtAuthenticator)
 
 providersRouter.get('/', providersController.index)
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index
+)
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index
+)
 
 export default providersRouter
