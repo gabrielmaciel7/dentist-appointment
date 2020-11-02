5e4fc5556a601d4081862fc52f92848ca2e3b788
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-02 14:54:50 -0300

    add provider appointments route

--- server/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ server/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController'
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController'
 import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController'
 import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController'
 
@@ -9,12 +10,15 @@ import jwtAuthenticator from '@modules/users/infra/http/middlewares/jwtAuthentic
 const providersRouter = Router()
 
 const providersController = new ProvidersController()
+const providerAppointmentsController = new ProviderAppointmentsController()
 const providerMonthAvailabilityController = new ProviderMonthAvailabilityController()
 const providerDayAvailabilityController = new ProviderDayAvailabilityController()
 
 providersRouter.use(jwtAuthenticator)
 
 providersRouter.get('/', providersController.index)
+
+providersRouter.get('/me', providerAppointmentsController.index)
 
 providersRouter.get(
   '/:provider_id/month-availability',
