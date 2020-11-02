d034d921869d5c7132bc341503e5c5c4185aa872
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-02 15:22:33 -0300

    config mongodb

--- server/src/shared/infra/typeorm/index.ts
+++ server/src/shared/infra/typeorm/index.ts
@@ -1,16 +1,29 @@
-import { createConnection } from 'typeorm'
+import { createConnections } from 'typeorm'
 
-createConnection({
-  name: 'default',
-  type: 'postgres',
-  host: process.env.TYPEORM_HOST,
-  port: Number(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: [process.env.TYPEORM_ENTITIES],
-  migrations: [process.env.TYPEORM_MIGRATIONS],
-  cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR
+createConnections([
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.TYPEORM_POSTGRES_HOST,
+    port: Number(process.env.TYPEORM_POSTGRES_PORT),
+    username: process.env.TYPEORM_POSTGRES_USERNAME,
+    password: process.env.TYPEORM_POSTGRES_PASSWORD,
+    database: process.env.TYPEORM_POSTGRES_DATABASE,
+    entities: [process.env.TYPEORM_POSTGRES_ENTITIES],
+    migrations: [process.env.TYPEORM_POSTGRES_MIGRATIONS],
+    cli: {
+      migrationsDir: process.env.TYPEORM_POSTGRES_MIGRATIONS_DIR
+    }
+  },
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.TYPEORM_MONGODB_HOST,
+    port: Number(process.env.TYPEORM_MONGODB_PORT),
+    username: process.env.TYPEORM_MONGODB_USERNAME,
+    password: process.env.TYPEORM_MONGODB_PASSWORD,
+    database: process.env.TYPEORM_MONGODB_DATABASE,
+    useUnifiedTopology: true,
+    entities: [process.env.TYPEORM_MONGODB_ENTITIES]
   }
-})
+])
