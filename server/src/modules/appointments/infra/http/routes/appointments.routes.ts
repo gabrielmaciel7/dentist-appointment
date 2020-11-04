458be39ec5c5aeb7c10aab92422b6b758e1258cf
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-04 09:37:43 -0300

    add create appointments validator

--- server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController'
 import jwtAuthenticator from '@modules/users/infra/http/middlewares/jwtAuthenticator'
@@ -8,6 +9,15 @@ const appointmentsController = new AppointmentsController()
 
 appointmentsRouter.use(jwtAuthenticator)
 
-appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date()
+    }
+  }),
+  appointmentsController.create
+)
 
 export default appointmentsRouter
--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { errors } from 'celebrate'
 
 import routes from './routes'
 
@@ -22,6 +23,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
+
+app.use(errors())
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
