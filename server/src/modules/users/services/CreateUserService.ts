c890bb983a36837b839e667c14d265c7897af5cb
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-14 09:55:06 -0300

    add invalidate prefix redis method

--- server/src/modules/users/services/CreateUserService.ts
+++ server/src/modules/users/services/CreateUserService.ts
@@ -7,6 +7,7 @@ import AppError from '@shared/errors/AppError'
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider'
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 interface IRequest {
   name: string
@@ -22,7 +23,10 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
 
     @inject('HashProvider')
-    private hashProvider: IHashProvider
+    private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({
@@ -45,6 +49,8 @@ class CreateUserService {
       password: hashedPassword,
       avatar
     })
+
+    await this.cacheProvider.invalidatePrefix('providers-list')
 
     return user
   }
