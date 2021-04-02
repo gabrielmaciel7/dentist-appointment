02edbbbd71c5a66154237e17d6c1624b7559ca4b
Gabriel Soares <gsoares@vitait.com>
2021-04-02 11:46:15 -0300

    Remove rate limiter from files

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -20,10 +20,10 @@ dotenv.config()
 
 const app = express()
 
-app.use(rateLimiter)
 app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
+app.use(rateLimiter)
 app.use(routes)
 
 app.use(errors())
